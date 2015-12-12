7bdc715fd39bf842ae5c9dc80a83b6d092b2f874
Hsiang Lin <hsiangl@andrew.cmu.edu>
2015-12-11 16:44:22 -0800

    Market Page Debugged

--- app/src/main/java/com/example/chloe/bceo/view/MarketActivity.java
+++ app/src/main/java/com/example/chloe/bceo/view/MarketActivity.java
@@ -23,71 +23,6 @@ public class MarketActivity extends AppCompatActivity {
     private static final String OUTPUT_FILE= Environment.getExternalStorageDirectory().getPath() + "/recordoutput.3gpp";
     private MediaPlayer mediaPlayer;
 
-    // references to our images
-    private Integer[] mThumbIds = {
-            R.drawable.androider_02,
-            R.drawable.androider_04,
-            R.drawable.androider_06,
-            R.drawable.androider_08,
-//            R.drawable.androider_10,
-//            R.drawable.androider_11,
-//            R.drawable.androider_12,
-//            R.drawable.androider_13,
-//            R.drawable.androider_14,
-//            R.drawable.androider_15,
-//            R.drawable.androider_16
-    };
-
-    public class MyAdapter extends BaseAdapter {
-
-        private Context mContext;
-
-        public MyAdapter(Context c) {
-            // TODO Auto-generated constructor stub
-            mContext = c;
-        }
-
-        @Override
-        public int getCount() {
-            // TODO Auto-generated method stub
-            return mThumbIds.length;
-        }
-
-        @Override
-        public Object getItem(int arg0) {
-            // TODO Auto-generated method stub
-            return mThumbIds[arg0];
-        }
-
-        @Override
-        public long getItemId(int arg0) {
-            // TODO Auto-generated method stub
-            return arg0;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            // TODO Auto-generated method stub
-
-            View grid;
-
-            if(convertView==null){
-                grid = new View(mContext);
-                LayoutInflater inflater=getLayoutInflater();
-                grid=inflater.inflate(R.layout.fragment_grid_item, parent, false);
-            }else{
-                grid = (View)convertView;
-            }
-
-            ImageView imageView = (ImageView) grid.findViewById(R.id.imagepart);
-            TextView textView = (TextView)grid.findViewById(R.id.textpart);
-            imageView.setImageResource(mThumbIds[position]);
-            textView.setText(String.valueOf(position));
-
-            return grid;
-        }
-
-    }
 
     /** Called when the activity is first created. */
     @Override
@@ -95,25 +30,12 @@ public class MarketActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market);
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new MyAdapter(this));
-        gridview.setOnItemClickListener(new ItemClickListener());
-
-        ImageView iv = (ImageView) findViewById(R.id.imageView_back_arrow);
-
         try {
             playRecording();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        iv.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(v.getContext(), BrowseActivity.class);
-                startActivityForResult(myIntent, 0);
-            }
-        });
     }
     public class ItemClickListener implements AdapterView.OnItemClickListener {
         @Override
