240c21aebac59a2aaa267eb3caa6d11bf8d53d5a
Chloe Shim <woojoo1210@gmail.com>
2015-12-11 20:33:44 -0800

    error handling for groups

--- app/src/main/java/com/example/chloe/bceo/view/GroupsActivity.java
+++ app/src/main/java/com/example/chloe/bceo/view/GroupsActivity.java
@@ -139,13 +139,19 @@ public class GroupsActivity extends AppCompatActivity {
 
                 StringBuilder tail =
                         new StringBuilder(
-                                "leave_group?user_email=" + user.getUserEmail());
+                                "leave_group?user_email=" + user.getUserEmail()
+                                + "group_id=" + user.getGroupID());
                 String urlStr = httpUtil.buildURL(tail.toString());
                 String response = httpUtil.getResponse(urlStr);
 
                 if (!response.equals("left")) {
                     Toast.makeText(GroupsActivity.this,
                             "Oops! unable to leave group, try again!",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                } else if (response.equals("invalid")) {
+                    Toast.makeText(GroupsActivity.this,
+                            "You don't belong this group, reload and try again!",
                             Toast.LENGTH_LONG).show();
                     return;
                 }
